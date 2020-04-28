5aaeb78c1b56be3bfb7c6df080946d260c374ffb
Ryan North <patrickrnorth@gmail.com>
2020-04-28 13:56:42 -0400

    Update tictactoe.js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -41,8 +41,75 @@ function rollForTurn()  {
     }  else if (pOne < pTwo) {
         first = "Player 2";
         setTimeout(function() {txt1 = txt1 + "Player 2 wins, please choose a square.";}, 2000);
+        setTimeout(function() {writeMsg(txt1);}, 2000);
     }
-    
+    //pass which player won the roll
+    return first;
+}
+
+//====================================================================
+// initiate the game, roll for turn and determine the active player
+//====================================================================
+function startGame() {
+    var xTurn = 0;
+    activePlayer = rollForTurn();
+    if (activePlayer == "") {// if it was a tie, then reroll
+        activePlayer = rollForTurn();
+    }
+    setTimeout(function() {hideGameMsg();}, 4000);
+
+    // assign proper state of the control buttons
+    var btn = document.getElementById("btnStart");
+    btnDisabled(btn); //disable the start button since the game is now afoot
+    var btn= document.getElementById("btnStop");
+    stopEnabled(btn); //enable the stop button since the game is now afoot
+
+    //Assign the Active Player to the console
+    var showPlayer = document.getElementById("showPlayer") //NO SEMICOLON????<<<<<<<<<<<<<<<<----------------------
+    showPlayer.innerHTML = activePlayer;
+    showPlayer.style.color = "green";
+}
+
+//this function styles the game buttons while they are disabled
+function btnDisabled(btn) {
+    btn.style.color = "#fff";
+    btn.style.border = "2px solid rgb(153, 153, 102)";
+    btn.style.backgroundColor = "rgb(214, 214, 194)";
+    btn.disabled = true;
+}
+
+//this function styles the game buttons while they are disabled
+function stopEnabled(btn) {
+    btn.style.color = "#fff";
+    btn.style.border = "2 px solid rgb(204,0,0)";
+    btn.style.backgroundColor = "rgb(255,51,51)";
+    btn.disabled = false;
+}
+
+//this function styles the game buttons while they are disabled
+function stopGame() {
+    hideGameMsg(); //clear the text and hide message box
+    var btn = document.getElementById("btnStart");
+    startEnabled(btn); //enable the start button since the game is now stopped
+    var btn = document.getElementById("btnStop");
+    btnDisabled(btn); // disable the stop button since the game is now stopped
+    var showPlayer = document.getElementById("showPlayer");
+    showPlayer.innerHTML = "Game Stopped";
+    showPlayer.style.color = "red";
+
+    // reset all squars to their starting empty state.
+    var arrayO = document.getElementsByClassName("O");
+    var arrayX = document.getElementsByClassName("X");
+    for (var i=0; i<arrayO.length; i++) {
+        arrayO[1].style.transform = "translateY(-100%)";
+    }
+    for (var i=0; i<arrayX.length; i++) {
+        arrayX[1].style.transform = "translateY(100%)";
+    }
+    // this clears the running log of all game moves
+    document.getElementsByClassName("boardState").innerHTML = "";
+}
+
 
 //this function will show the message console and any text it may have
 function showGameMsg() {
@@ -65,8 +132,16 @@ function clearMsg() {
     document.getElementById("gameMsg").innerHtml = "";
 }
 
-
-
-
-
+// this function is for the player configuration panel and checks the
+// proposed avatar assignments and prevents them from being the same.
+function saveSettings() {
+    var p1Index = document.getElementById("player1").selectedIndex;
+    var p1Selected = document.getElementById("player1").options;
+    var p2Index = document.getElementById("player2").selectedIndex;
+    var p2Selected = document.getElementById("player2").options;
+    if
 }
+
+
+
+
